f0d174a46e8f0c0bdfc10988dba246b52d76df46
Jecé Xavier <jece@alunos.utfpr.edu.br>
2023-06-06 16:23:12 -0300

    feat: add doc for Box

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Box, BoxProps } from '@ignite-ui-jx/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+
+  args: {
+    children: (
+      <>
+        <span>Testando o elemento Box</span>
+      </>
+    ),
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
